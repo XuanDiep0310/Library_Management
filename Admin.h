84a76881405dc75b870871c744f7474baf58b95e
Xuan Diep <diepp031005@gmail.com>
2024-10-05 10:03:09 +0700

    update Admin

--- Admin.h
+++ Admin.h
@@ -1,23 +1,25 @@
 #pragma once
-#include <iostream>
+#include<iostream>
+#include<iomanip>
+#include "LinkedList.h"
 #include "Person.h"
 #include "Book.h"
-#include "LinkedList.h"
-#include "User.h"
+#include "Access.h"
+using namespace std;
 
-class Admin : public Person 
+class Admin : public Person
 {
-    public:
-        Admin();
-        LinkedList<Book>* fixBook(LinkedList<Book>* lbook, LinkedList<User>* luser);
-        LinkedList<Book>* addBook(LinkedList<Book>* lbook);
-        LinkedList<Book>* delBook(LinkedList<Book>* lbook);
-        void displayUser(LinkedList<User>* luser);
-        list delUser(list l);
-        void findUser(LinkedList<User>* luser, LinkedList<Book>* lbook);
-        void borrowBook(list l, int time);
-        void giveBackBook(list l, int money);
-        void resetPasswork(LinkedList<User>* luser);
-        void updateIF(LinkedList<User>* luser);
-        void thongKe(LinkedList<Book>* lbook,LinkedList<User>* luser);
+public:
+    Admin();
+    LinkedList<Book>* fixBook(LinkedList<Book>* lbook, LinkedList<User>* luser);
+    LinkedList<Book>* addBook(LinkedList<Book>* lbook);
+    LinkedList<Book>* delBook(LinkedList<Book>* lbook);
+    void displayUser(LinkedList<User>* luser);
+    list delUser(list l);
+    void findUser(LinkedList<User>* luser, LinkedList<Book>* lbook);
+    void borrowBook(list l, int time);
+    void giveBackBook(list l, int money);
+    void resetPasswork(LinkedList<User>* luser);
+    void updateInfor(LinkedList<User>* luser);
+    void statistic(LinkedList<Book>* lbook, LinkedList<User>* luser);
 };
